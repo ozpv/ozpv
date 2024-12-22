4408143e2db3779ba68f3ca722a7072244427b53
ozpv <39195175+ozpv@users.noreply.github.com>
2024-12-22 03:28:38 -0600

    add conditional rendering for small screens

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,8 @@
-use leptos::{ev, html, prelude::*, tachys::renderer::dom::Dom};
+use leptos::{
+    ev, html,
+    prelude::*,
+    tachys::{dom::window, renderer::dom::Dom},
+};
 use leptos_icons::Icon;
 use leptos_meta::{provide_meta_context, MetaTags, Stylesheet, Title};
 use leptos_router::{
@@ -26,7 +30,7 @@ pub fn shell(options: LeptosOptions) -> impl IntoView {
 }
 
 #[component]
-fn HomePage() -> impl IntoView {
+fn DesktopHomePage() -> impl IntoView {
     use std::cmp::Ordering;
 
     let rustacean = NodeRef::<html::Div>::new();
@@ -34,10 +38,7 @@ fn HomePage() -> impl IntoView {
     let right_eye = NodeRef::new();
 
     // set position of center of eye hole relative to parent
-    let eyes = [
-        (right_eye, (260.0, 218.0)),
-        (left_eye, (386.0, 218.0)),
-    ];
+    let eyes = [(right_eye, (260.0, 218.0)), (left_eye, (386.0, 218.0))];
 
     // on mousemove
     let mousemove_handle = window_event_listener(ev::mousemove, move |ev| {
@@ -55,7 +56,7 @@ fn HomePage() -> impl IntoView {
         eyes.iter()
             .filter_map(|(eye, positions)| eye.get().map(|eye| (eye, positions)))
             .for_each(|(eye, positions): (HtmlImageElement, &(f64, f64))| {
-                let dx = px - (positions.0+ parent_x);
+                let dx = px - (positions.0 + parent_x);
                 let dy = py - (positions.1 + parent_y);
                 let r = dx.hypot(dy);
                 let sin = dx.abs() / r;
@@ -91,13 +92,46 @@ fn HomePage() -> impl IntoView {
     on_cleanup(move || mousemove_handle.remove());
 
     view! {
+        <div class="relative w-[749px] h-[435px] m-auto translate-x-6" node_ref=rustacean>
+            <img src="rustacean-no-eyes.png" class="z-10 absolute"/>
+            <img src="rustacean-eye.png" class="z-0 absolute" style="left:260px;top:218px;" node_ref=right_eye/>
+            <img src="rustacean-eye.png" class="z-0 absolute" style="left:386px;top:218px;" node_ref=left_eye/>
+        </div>
+    }
+}
+
+#[component]
+fn MobileHomePage() -> impl IntoView {
+    view! {
+        <img src="rustacean.png" class="mx-auto"/>
+    }
+}
+
+#[component]
+fn HomePage() -> impl IntoView {
+    let small = RwSignal::new(false);
+
+    Effect::new(move || {
+        let Some(width) = window()
+            .inner_width()
+            .expect("window to have a width")
+            .as_f64()
+        else {
+            return;
+        };
+
+        // rustacean width plus the translation of 6.0
+        if width < 755.0 {
+            small.set(true);
+        }
+    });
+
+    view! {
         <div class="bg-black min-h-screen">
-            <h1 class="text-8xl text-center text-white py-24">ozpv</h1>
-            <div class="relative w-[749px] h-[435px] m-auto translate-x-6" node_ref=rustacean>
-                <img src="rustacean-no-eyes.png" class="z-10 absolute"/>
-                <img src="rustacean-eye.png" class="z-0 absolute" style="left:260px;top:218px;" node_ref=right_eye/>
-                <img src="rustacean-eye.png" class="z-0 absolute" style="left:386px;top:218px;" node_ref=left_eye/>
-            </div>
+            <h1 class="text-6xl text-center text-white py-24 sm:text-8xl">ozpv</h1>
+            <Show when=move || small.get() fallback=DesktopHomePage>
+                <MobileHomePage />
+            </Show>
             <div class="flex justify-center gap-5 pt-14">
                 <a href="https://github.com/ozpv" class="p-2 rounded-sm transition-all ease-in duration-150 hover:-translate-y-px hover:bg-slate-800">
                     <Icon icon={icondata::SiGithub} width="32" height="32" {..} class="text-white" />
