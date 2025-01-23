eb7d2dde0a7aa7d4af9780e0cc413bf3d8492278
ozpv <39195175+ozpv@users.noreply.github.com>
2025-01-23 03:23:30 -0600

    update deps

--- ozpv.nix
+++ ozpv.nix
@@ -2,12 +2,21 @@
 
 pkgs.mkShell {
   packages = with pkgs; [
-    rustup
+	rustc
+	cargo
+	clippy
+	rustfmt
+	lld
     cargo-binutils
     cargo-leptos
     cargo-generate
     tailwindcss
     binaryen
+    postgresql
+    sqlx-cli
+    pkg-config
+    twiggy
+    openssl
 	wasm-bindgen-cli
   ];
 }
